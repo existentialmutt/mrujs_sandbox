59a435727631206e2a25854c189fe70f7dff960f
Rafe Rosen <rafe@existentialmutt.com>
2021-07-17 13:01:17 -0400

    mrujs scaffold

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,17 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import mrujs from "mrujs";
+// import * as Turbo from "@hotwired/turbo";
+// window.Turbo = Turbo;
+// mrujs.start();
+
+
+import Turbolinks from "turbolinks";
+
+mrujs.start();
+Turbolinks.start();
+
+Rails.start()
